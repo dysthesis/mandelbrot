024744f3f8c207861304c72df323ab359f0957b2
Dysthesis <antheoraviel@protonmail.com>
2025-02-28 17:06:43 +1100

    Changes

--- src/xmonad/lib/Config/XMobar.hs
+++ src/xmonad/lib/Config/XMobar.hs
@@ -1,40 +1,43 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
-module LogHook (
-    withStatusBars,
-    myLogHook,
-)
-where
+module Config.XMobar (xmobarProp) where
 
-import Layout (ppLayoutOverride)
-import XMonad
-import XMonad.Hooks.StatusBar
-import XMonad.Hooks.StatusBar.PP
+import XMonad (KeyMask, KeySym, Layout, XConfig (XConfig, modMask), xK_b)
+import XMonad.Hooks.DynamicLog (PP (ppSort), xmobarPP)
+import XMonad.Hooks.StatusBar (StatusBarConfig, dynamicSBs, statusBarProp, statusBarPropTo, withEasySB)
+import XMonad.Hooks.StatusBar.PP (PP (ppCurrent, ppExtras, ppHidden, ppLayout, ppOrder, ppOutput, ppSep, ppTitle, ppTitleSanitize, ppUrgent, ppVisible, ppVisibleNoWindows, ppWsSep), def, filterOutWsPP, shorten, wrap, xmobarBorder, xmobarColor, xmobarFont, xmobarStrip)
+import XMonad.Util.NamedScratchpad (scratchpadWorkspaceTag)
 
-------------------------------------------------------------------------
--- Status bars and logging
-
--- Perform an arbitrary action on each internal state change or X event.
--- See the 'XMonad.Hooks.StatusBar' extension for examples.
-
-myLogHook :: X ()
-myLogHook = return ()
-
-withStatusBars :: (LayoutClass l Window) => XConfig l -> XConfig l
-withStatusBars = dynamicSBs barSpawner
-
-barSpawner :: ScreenId -> IO StatusBarConfig
-barSpawner = pure . xmobar
+xmobarProp = withEasySB (statusBarProp "xmobar -x 0 ~/.config/xmobar/xmobar.hs" (pure (filterOutWsPP [scratchpadWorkspaceTag] myXmobarPP))) toggleStrutsKey
   where
-    pp :: PP
-    pp =
-        def
-            { ppCurrent = xmobarColor "#f9e2af" "" . wrap "[" "]"
-            , ppTitle = xmobarColor "#cba6f7" "" . shorten 40
-            , ppVisible = wrap "(" ")"
-            , ppUrgent = xmobarColor "#f38ba8" "#f9e2af"
-            , ppSep = " \57533 "
-            , ppLayout = ppLayoutOverride
-            }
-    xmobar :: ScreenId -> StatusBarConfig
-    xmobar (S screenId) = statusBarProp ("xmobar -x " <> show screenId) $ pure pp
+    toggleStrutsKey :: XConfig Layout -> (KeyMask, KeySym)
+    toggleStrutsKey XConfig {modMask = m} = (m, xK_b)
+
+myXmobarPP :: PP
+myXmobarPP =
+  def
+    { ppSep = grey "  \xf01d9  ",
+      ppCurrent = blue . wrap "" "" . xmobarBorder "Top" "#89b4fa" 3,
+      -- , ppCurrent = blue
+      ppHidden = grey,
+      ppVisible = white,
+      ppWsSep = "  ",
+      ppTitleSanitize = xmobarStrip . shorten 30, -- `shorten` defines the max length
+      ppTitle = wrap "\xf0570 " "",
+      ppLayout =
+        white
+          . ( \case
+                "Spacing Tabbed Tall" -> "<icon=tiled.xpm/>"
+                "Mirror Spacing Tabbed Tall" -> "<icon=mirrortiled.xpm/>"
+                "Full" -> "<icon=full.xpm/>"
+                "monocle" -> "<icon=monocle.xpm/>"
+                "Spacing ThreeCol" -> "<icon=threecol.xpm/>"
+                "Tabbed Simplest" -> "<icon=tabbed.xpm/>"
+                "BSP" -> "<icon=bsp.xpm/>"
+            )
+    }
+  where
+    grey = xmobarColor "#6c7086" ""
+    white = xmobarColor "#ffffff" ""
+    blue = xmobarColor "#89b4fa" ""
