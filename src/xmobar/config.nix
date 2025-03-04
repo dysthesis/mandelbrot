f45aa7f02156d1cfefafcfd0030e5506a5aec8c2
Dysthesis <antheoraviel@protonmail.com>
2025-03-04 11:24:35 +1100

    Whoops

--- src/xmobar/config.nix
+++ src/xmobar/config.nix
@@ -20,7 +20,7 @@ in
           iconOffset = -1,
           position = BottomH 32,
           commands =
-            [ Run XPropertyLog "_XMONAD_LOG",
+            [ Run XMonadLog,
               Run $
                 WeatherX
                   "YSSY"
